7ba05562b8a9f47dc12be039b6247cda96008827
Caitlin Wall <caitlinwall96@gmail.com>
2021-09-26 16:07:32 -0400

    Add city name, date with moment, and icon for weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,42 @@
 var APIKey = "65566f3f953d22f68075807f0afd5b8b";
 var city;
-var searchButton = document.getElementById('search-button');
+var searchButton = document.getElementById("search-button");
+var currentCity = document.getElementById("currentCity");
+var weatherIcon = document.getElementById("weatherIcon");
+var mainPage = document.getElementById("mainPage");
 
-function saveCity() {
-    var input = document.getElementById("city").value.trim();
-    city = input;
-}
+searchButton.addEventListener("click", getApi);
 
 function getApi() {
+    var input = document.getElementById("city").value.trim();
+    city = input;
     var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    // fetch request gets a list of all the cities and their data
+    var today = moment();
+    
+    // fetch request gets the data for the city
     fetch(cityUrl)
       .then(function (response) {
+        if (response.status !== 200) {
+            alert("That city is not valid");
+        }
         return response.json();
       })
       .then(function (data) {
         console.log(data);
+        var iconcode = data.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        mainPage.className = "";
+        $('#weatherIcon').attr('src', iconURL);
+        currentCity.textContent = data.name;
+        $("#currentTime").text(today.format("(MM/DD/YY)"));
       });
-  }
+}
+
+function saveCities() {
+
+}
   
 
-  searchButton.addEventListener('click', saveCity);
-  searchButton.addEventListener('click', getApi);
+  
 
   
